e3bc15007cabe31f431e60c26db12fa6125ec8af
mthibault006 <thibault.maxime@outlook.com>
2021-12-03 14:06:21 +0100

    correction boutons blind jetons

--- src/components/Blind/index.js
+++ src/components/Blind/index.js
@@ -13,8 +13,8 @@ const Blind= () => {
             <div className="container-fluid justify-content-center">
                 <input type="number" value={jetons} readOnly />
                 <Button className="btn btn-outline-light">Passer</Button>
-                <Button className="btn btn-light" action={miser}>Miser (1 jetons)</Button>
-                <Button className="btn btn-light" action={allIn}>All-In (5 jetons)</Button>
+                <Button className="btn btn-light" onClick={miser}>Miser (1 jetons)</Button>
+                <Button className="btn btn-light" onClick={allIn}>All-In (5 jetons)</Button>
             </div>
         </div>
     )
